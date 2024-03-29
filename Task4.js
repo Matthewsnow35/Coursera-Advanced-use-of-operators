845273e8c18ecf32db8937010aeabbd4afd5bd9c
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2024-03-29 01:21:36 -0400

    Create Task4.js
    Coursera activity

---
+++ Task4.js
@@ -0,0 +1,6 @@
+/* Task 4: Add numbers using the + operator
+Console log the result of adding two numbers, 5 and 10, using the + operator.
+
+Note: This task should be completed on a single line of code. The output in the console should be 15. */ 
+----------------------------------------------------------------------------------------------------------
+console.log(5 + 10)
