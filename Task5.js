20f033ad23cedfdde7a92aa59ee26a2b4d5ee2c5
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2024-03-29 03:30:51 -0400

    Create Task5.js
    Coursera lab activity

---
+++ Task5.js
@@ -0,0 +1,19 @@
+/* Code three variables:
+
+The first variable should be a string with the following value: "Now in ". Name the variable now.
+
+The second variable should be a number with the value: 3. Name the variable three.
+
+The third variable should a string with the following value: "D!". Name the variable d.
+
+Console log the following code: now + three + d.
+
+Note: The expected output should be: "Now in 3D!". 
+
+
+Results: */
+
+var now = "Now in "
+var three = "3"
+var d = "3D!"
+console.log("Now in 3D!")
